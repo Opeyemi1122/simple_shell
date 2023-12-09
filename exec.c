5d3d526dfa69aa640dbc84c8bd9bf439474ebfd6
Opeyemi1122 <alabiopex@gmail.com>
2023-12-09 20:30:13 +0100

    fork and exec file created

--- exec.c
+++ exec.c
@@ -2,19 +2,20 @@
 #include <unistd.h>
 
 /**
- * main - Execve example
+ * main - execve example
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	char *argv[] = {"/bin/ls", "-l", "/usr/",NULL};
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 
+	printf("Before execve\n");
 	if (execve(argv[0], argv, NULL) == -1)
 	{
 		perror("Error:");
 	}
-
+	printf("After execve\n");
 	return (0);
 }
