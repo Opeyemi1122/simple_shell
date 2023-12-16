7cd1b268f94262b1c87a5dc07bdc20b9034278bf
Yinka Akindele <akindeleyinka100@gmail.com>
2023-12-15 20:16:18 -0800

    Changing the prompt sign in an attempt to fix task 5

--- shell.c
+++ shell.c
@@ -15,7 +15,7 @@ void shell_loop(runtime_data *datash)
 	loop = 1;
 	while (loop == 1)
 	{
-		write(STDIN_FILENO, "#cisfun$ ", 9);
+		write(STDIN_FILENO, "$ ", 2);
 		input = read_line(&i_eof);
 		if (i_eof != -1)
 		{
